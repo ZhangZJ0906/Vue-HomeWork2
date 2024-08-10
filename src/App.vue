c39fe198500d6966553514f30ba79584d4c3ea20
ZhangZJ0906 <cklgy88@gmail.com>
2024-08-11 00:06:14 +0800

    更新註冊登入介面 api 尚未連接

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>註冊</h1>
+        <form>
+          <div class="mb-3 my-4">
+            <label for="email1" class="form-label">email</label>
+            <input type="text" id="email1" placeholder="email" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="Password1" class="form-label">Password</label>
+            <input type="password" id="Password1" class="form-control" placeholder="password">
+          </div>
+          <div class="mb-3">
+            <label for="nickname" class="form-label">nickname</label>
+            <input type="password" id="nickname" class="form-control" placeholder="nickname">
+          </div>
+        </form>
+        <div id="SignupReturnMessage"></div>
+      </div>
+
+      <div class="col-md-6">
+        <h1>登入</h1>
+        <form>
+          <div class="mb-3">
+            <label for="email2" class="form-label">email</label>
+            <input type="text" id="email2" placeholder="email" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="Password2" class="form-label">Password</label>
+            <input type="password" id="Password2" class="form-control" placeholder="password">
+          </div>
+        </form>
+        <div id="LoginReturnMessage"></div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  name: 'App'
+
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style>
+body {
+  background-color: #ffffff;
 }
 </style>
